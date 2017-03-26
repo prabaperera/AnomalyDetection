d23953d230741be354db6ec910fbb01d103009bc
chamini4me <chamini4me@gmail.com>
2017-03-27 00:18:26 +0800

    updated

--- Feature_Extraction.py
+++ Feature_Extraction.py
@@ -21,56 +21,7 @@ def read_file(filename_rd):
             process_line(line)    
 
 
-#test_file
-
-def load_x_test_redteam (filename) :
-
-    list_data=[[]]
-    
-    j=0
-    with open(filename) as f:
-        for line in f:
-            array_split=line.split('[')
-            array_split=array_split[0].split(']')            
-            array_split=array_split[0].split(',')
-            for i in array_split:        
-                list_data[j].append(float(i))
-            list_data.append([])
-            j=j+1
-    
-    list_data.pop()    
-    return list_data
-
-    
-def load_x_train () :
-
-    list_data=[[]]
-    list_str=[]
-    
-    j=0
-    with open(wr_file) as f:
-        for line in f:
-            list_str.append(line)
-            array_split=line.split(',')            
-            for i in array_split:        
-                list_data[j].append(float(i))
-            list_data.append([])
-            j=j+1
-    
-    list_data.pop()
-    list_len=len(list_data)
-    test_set_len=int(list_len*.05)
-    train_set_len=list_len-test_set_len
    
-    while (len(list_data) > train_set_len ):
-        record=list_str[train_set_len-1]
-        test_wr_file.write(record)
-        del list_data[train_set_len-1]  
-        del list_str[train_set_len-1]
-    return list_data
-
-
-
 
 def write_auth (key) :
     
@@ -287,38 +238,10 @@ def redteam_model_if():
 
     y_pred_outliers = clf.predict(X_outliers)
     n_error_outliers = y_pred_outliers[y_pred_outliers == 1].size
-    print(y_pred_outliers.size)
-    print(n_error_outliers)
+    print("total outliners: "+ str(len(X_outliers)))
+    print("error in outliners: "+ str(n_error_outliers))
 
     
-def test_model_if():
-    X_test=load_x_test_redteam(test_file)
-    #X_outliers=load_x_outliner()
-    dump_file=dir_base+'/model_dump/model_if.pkl'
-    if os.path.isfile(dump_file):
-        clf = joblib.load(dump_file)
-    else:
-        print('error in loading model')
-
-    y_pred_test = clf.predict(X_test)
-    n_error_test = y_pred_test[y_pred_test == -1].size
-    print(len(X_test))
-    print(n_error_test)
-    
-    
-          
-def train_model_if():
-    rng = None     
-    X_train=load_x_train()
-    dump_file=dir_base+'/model_dump/model_if.pkl'
-    if os.path.isfile(dump_file):
-        clf = joblib.load(dump_file)
-    else:
-        clf = IsolationForest(max_samples=100, random_state=rng)
-    
-    clf.fit(X_train)
-    joblib.dump(clf,dump_file) 
-
 
 def setup():
     global list_auth_type
@@ -360,7 +283,7 @@ list_st_end=['Start','End']
 
 dir_read_train='E:/AI/train/'
 dir_base='E:/AI'
-wr_file=dir_base+'/temp_feactures/fect_train.txt'   
+wr_file=dir_base+'/temp_feactures/fect_train_lastset.txt'   
 test_file=dir_base+'/temp_feactures/fect_test.txt'  
 redteam_file=dir_base+'/temp_feactures/fect_readteam.txt' 
 test_wr_file=open(test_file,"w")
@@ -370,20 +293,20 @@ setup()
 #file_wr_auth = open(filename_wr_auth,"w")
 file_wr_auth=None
 filenames=os.listdir(dir_read_train)
+filenames.sort()
 print('all the filenames in the directory: '+ str(filenames))
 
 for i in filenames:
-    i=dir_read_train+i   
-     
+    i_array=i.split('.')
+    wr_file=dir_base+'/temp_feactures/'+i+'_feactures.txt'  
+    
+    i=dir_read_train+i
     file_wr_auth=open(wr_file,"w")
-    print('file: '+i +' reading......')
+    print('file: '+i +' reading started......')
     read_file (i)
     file_wr_auth.close()
-    print('file: '+i +' train started......')
-    train_model_if()
-    print('file: '+i +' train end......')
+    print('file: '+i +' reading ended......')
     
-
 #to write the last set
 print('writing last set')
 #key_list=list(set_key)
@@ -392,21 +315,3 @@ for item in set_key:
     write_auth(item)
 file_wr_auth.close()
 print('traing last set started')
-train_model_if()
-print('traing last set ended')
-
-
-print('model testing started..')
-test_wr_file.close()
-test_model_if()
-
-
-##load readteam
-set_key=set()
-file_wr_auth=open(redteam_file,"w")
-print('file: '+i +' reading......')
-read_file (dir_base+'/red_team/readteam.txt')
-for item in set_key:
-    write_auth(item)
-redteam_model_if()
-file_wr_auth.close()
